d80b788e6399ddda0a61928167ce16e99c12b6fc
Gabriel Arroyo <gabriel_eleazar.arroyo@daimler.com>
2022-09-09 18:02:45 -0500

    contact responsive

--- js/scripts.js
+++ js/scripts.js
@@ -32,8 +32,109 @@ window.addEventListener("DOMContentLoaded", (event) => {
 });
 
 let muted = true;
-function mute() {
+function toggleMute() {
   var vid = document.getElementById("bg-video");
   vid.muted = !muted;
   muted = vid.muted;
 }
+function mute() {
+  var vid = document.getElementById("bg-video");
+  vid.muted = true;
+  muted = vid.muted;
+}
+function unmute() {
+  var vid = document.getElementById("bg-video");
+  vid.muted = false;
+  muted = vid.muted;
+}
+
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+const unmuteStart = async () => {
+  await delay(5000);
+  console.log("Waited 5s");
+  mute();
+};
+
+function reveal() {
+  try {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function appear() {
+  try {
+    var reveals = document.querySelectorAll(".appear");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function spin() {
+  try {
+    var reveals = document.querySelectorAll(".spin");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function slide() {
+  try {
+    var reveals = document.querySelectorAll(".slide");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+window.addEventListener("scroll", reveal);
+window.addEventListener("scroll", appear);
+window.addEventListener("scroll", spin);
+window.addEventListener("scroll", slide);
+
+// To check the scroll position on page load
+reveal();
+appear();
+spin();
+slide();
